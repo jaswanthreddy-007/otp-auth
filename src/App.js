dffe19f9f51d5d04a40c5c78718df0bb472f319c
jaswanthreddy-007 <88081034+jaswanthreddy-007@users.noreply.github.com>
2022-12-11 00:48:35 +0530

    otp_working

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ class App extends React.Component {
     window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('sign-in-button', {
       'size': 'invisible',
       'callback': (response) => {
-        // reCAPTCHA solved, allow signInWithPhoneNumber.
         this.onSignInSubmit();
         console.log("Recaptca varified")
       },
@@ -27,14 +26,12 @@ class App extends React.Component {
     const appVerifier = window.recaptchaVerifier;
     firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
       .then((confirmationResult) => {
-        // SMS sent. Prompt user to type the code from the message, then sign the
-        // user in with confirmationResult.confirm(code).
+       
         window.confirmationResult = confirmationResult;
         console.log("OTP has been sent")
-        // ...
+        
       }).catch((error) => {
-        // Error; SMS not sent
-        // ...
+       
         console.log("SMS not sent")
       });
   }
@@ -43,14 +40,13 @@ class App extends React.Component {
     const code = this.state.otp
     console.log(code)
     window.confirmationResult.confirm(code).then((result) => {
-      // User signed in successfully.
+      
       const user = result.user;
       console.log(JSON.stringify(user))
       alert("User is verified")
-      // ...
+     
     }).catch((error) => {
-      // User couldn't sign in (bad verification code?)
-      // ...
+     
     });
   } 
   render() {
